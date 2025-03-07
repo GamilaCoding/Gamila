ed8574fcdfaa4fc3b44da21fb128819da9f02307
Frontend developer <123306614+GamilaCoding@users.noreply.github.com>
2025-03-07 21:30:12 +0200

    Update seo.config.ts

--- app/seo.config.ts
+++ app/seo.config.ts
@@ -1,22 +1,13 @@
 const SEO = {
-          title: "Gamila | Web Developer",
-          description: "Crafting digital experiences with passion. Let's bring your ideas to life!",
-          canonical: "https://gamila-one.vercel.app/",
-          openGraph: {
-            url: "https://gamila-one.vercel.app/",
-            title: "Gamila | Web Developer",
-            description: "Crafting digital experiences with passion.",
-            images: [
-              {
-                url: "/og-image.png",
-                width: 800,
-                height: 600,
-                alt: "Gamila Portfolio",
-              },
-            ],
-            siteName: "Gamila | Web Developer",
-          },
-        };
-        
-        export default SEO;
-        
+  title: "Gamila | Frontend Web Developer with SEO Expertise",
+  description: "Crafting seamless web experiences with SEO best practices to boost your online presence.",
+  keywords: "Frontend Web Developer, SEO, React, Next.js, Web Design",
+  openGraph: {
+    title: "Gamila | Web Developer",
+    description: "Crafting seamless web experiences with SEO best practices to boost your online presence.",
+    url: "https://gamila-one.vercel.app/",
+    type: "website",
+  }
+};
+
+export default SEO;
