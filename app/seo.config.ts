08097bc2eeea3f8037ac8149c0856da9b10fb26a
Frontend developer <123306614+GamilaCoding@users.noreply.github.com>
2025-03-07 21:24:33 +0200

    Add files via upload

---
+++ app/seo.config.ts
@@ -0,0 +1,22 @@
+const SEO = {
+          title: "Gamila | Web Developer",
+          description: "Crafting digital experiences with passion. Let's bring your ideas to life!",
+          canonical: "https://gamila-one.vercel.app/",
+          openGraph: {
+            url: "https://gamila-one.vercel.app/",
+            title: "Gamila | Web Developer",
+            description: "Crafting digital experiences with passion.",
+            images: [
+              {
+                url: "/og-image.png",
+                width: 800,
+                height: 600,
+                alt: "Gamila Portfolio",
+              },
+            ],
+            siteName: "Gamila | Web Developer",
+          },
+        };
+        
+        export default SEO;
+        
